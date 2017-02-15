c1e79b9059cce13a05003fd3132370219c6a7f24
Mikhail Barsukou <mikhail_barsukou@epam.com>
2017-02-15 13:03:29 +0300

    Abiturient list - task #4 - added

--- ShapeProcessor/src/com/study/calculator/shapecalc/utils/Constants.java
+++ ShapeProcessor/src/com/study/calculator/shapecalc/utils/Constants.java
@@ -1,8 +1,8 @@
 package com.study.calculator.shapecalc.utils;
 
 public class Constants {
-	public static final String INPUT_PATH = "./resources/inFile.txt";
-	public static final String OUTPUT_PATH = "./resources/outFile.txt";
+	public static final String INPUT_PATH = "./resource/inFile.txt";
+	public static final String OUTPUT_PATH = "./resource/outFile.txt";
 	public static final String WRONG_INPUT_MESSAGE = "Please, enter a valid radius!";
 
 }
